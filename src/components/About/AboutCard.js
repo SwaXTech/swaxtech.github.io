85c2d66f92082eea0163319eaa65c2a32cf04d44
Alee <aleeherasimiuk@gmail.com>
2022-04-18 22:26:56 -0300

    Modifying about card

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
@@ -8,30 +7,26 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Alexis Herasimiuk </span>
+            from <span className="purple"> Buenos Aires, Argentina</span>
+            <br />I am a junior developer and studying Information Systems
+            Engineering at the UTN FRBA.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Currently, I am working as a Full Stack Developer & Mobile Developer at{" "}
+            <b>
+              <a
+                href="https://dblandit.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="purple"
+                style={{ textDecoration: "none" }}
+              >
+                {" "}
+                DBLandIT
+              </a>
+            </b>
           </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
-
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
         </blockquote>
       </Card.Body>
     </Card>
